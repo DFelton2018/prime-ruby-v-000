ba47c07223336ac972bc31ed616e75061b849f7f
Duane Felton <dfelton2010@gmail.com>
2018-10-09 13:27:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(n)
+  
+  primes = (0..n).to_a
+  primes[0] = nil
+  primes[1] = nil
+  
+  primes.each do |p|
+    next unless p
+    break if p*p > n
+    (p*p).step(n,p) { |x| primes[x] = nil }
+
+end
+end
